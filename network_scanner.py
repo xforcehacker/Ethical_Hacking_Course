aca949c1d5fe56c493880564b75e69beb56cf48d
Chaitanya <xforcehacker@users.noreply.github.com>
2021-07-03 11:40:59 -0400

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -4,13 +4,14 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcat = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_reques_broadcast = broadcat/arp_request
-    answered_list = scapy.srp(arp_reques_broadcast, timeout=1)[0]
+    #VERBOSE IS OFF DETAIL WILL NOT BE DISPLAYED
+    answered_list = scapy.srp(arp_reques_broadcast, timeout=1, verbose=False)[0]
+    print("-------------------------------------------")
+    print("IP\t\t\t MAC Address\n-------------------------------------------")
     for element in answered_list:
         #PRINT IP ADDRESS
-        print(element[1].psrc)
-        #PRINT MAC ID
-        print(element[1].hwsrc)
-        print("-------------------------------------------")
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        
 
 #SCANNING SUBNET
 scan("192.168.1.1/24")
